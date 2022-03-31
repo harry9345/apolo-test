c56516a470acd3550d25de4dd37ca095f423ceb9
Hossein movahed <hossein.movahed@monese.com>
2022-03-31 16:10:07 +0300

    small tweak

--- index.js
+++ index.js
@@ -2,114 +2,110 @@ const { ApolloServer, UserInputError, gql } = require('apollo-server')
 const { v1: uuid } = require('uuid')
 
 let persons = [
-    {
-        name: 'abbas re',
-        phone: '058-3-4245-58',
-        street: 'asddsa',
-        id: '0',
-    },
-    {
-        name: 'kare re',
-        phone: '058-2342-228',
-        street: 'gfdsgf',
-        id: '1',
-    },
-    {
-        name: 'asdfass re',
-        phone: '0584-4352-8',
-        street: 'ésgfsgfs',
-        id: '2',
-    },
-    {
-        name: 'abbfsgdsf re',
-        phone: '05-52345-435',
-        street: 'ésfdgss',
-        id: '3',
-    },
+  {
+    name: 'Arto Hellas',
+    phone: '040-123543',
+    street: 'Tapiolankatu 5 A',
+    city: 'Espoo',
+    id: '3d594650-3436-11e9-bc57-8b80ba54c431',
+  },
+  {
+    name: 'Matti Luukkainen',
+    phone: '040-432342',
+    street: 'Malminkaari 10 A',
+    city: 'Helsinki',
+    id: '3d599470-3436-11e9-bc57-8b80ba54c431',
+  },
+  {
+    name: 'Venla Ruuska',
+    street: 'Nallemäentie 22 C',
+    city: 'Helsinki',
+    id: '3d599471-3436-11e9-bc57-8b80ba54c431',
+  },
 ]
 
 const typeDefs = gql`
-type Address {
-    street:String!
-    city:String!
-}
-type Person {
-    name:String!
-    phone:String
-    address:Address!
-    id:ID!
-}
-type Mutation {
-    addPerson(
-    name:String!
-    phone:String
-    street:String!
-    city:String!
-    ):Person
-    editNumber(
-    name:String!
-    phone:String!
-    ):Person
-}
-
-enum YesNo {
-    Yes
+  type Address {
+    street: String!
+    city: String!
+  }
+  type Person {
+    name: String!
+    phone: String
+    address: Address!
+    id: ID!
+  }
+  enum YesNo {
+    YES
     NO
-}
-type Query {
-    personCount:Int!
-    allPersons(phone:YesNo):[Person!]!
-    findPerson(name:String!):Person
-}
+  }
+  type Query {
+    personCount: Int!
+    allPersons(phone: YesNo): [Person!]!
+    findPerson(name: String!): Person
+  }
+  type Mutation {
+    addPerson(
+      name: String!
+      phone: String
+      street: String!
+      city: String!
+    ): Person
+    editNumber(name: String!, phone: String!): Person
+  }
 `
 
 const resolvers = {
-    Query: {
-        personcount: () => persons.length,
-        allPersons: (root, args) => {
-            if (!args.phone) {
-                return persons
-            }
-            const byPhone = (person) =>
-                args.phone === 'YES' ? person.phone : !person.phone
-            return persons.filter(byPhone)
-        },
-        findPerson: (root, args) =>
-            persons.find(p => p.name === args.name)
+  Query: {
+    personCount: () => persons.length,
+    allPersons: (root, args) => {
+      if (!args.phone) {
+        return persons
+      }
+      const byPhone = (person) =>
+        args.phone === 'YES' ? person.phone : !person.phone
+      return persons.filter(byPhone)
     },
-    Person: {
-        address: ({ street, city }) => {
-            return {
-                street,
-                city
-            }
-        }
+    findPerson: (root, args) => persons.find((p) => p.name === args.name),
+  },
+  Person: {
+    address: ({ street, city }) => {
+      return {
+        street,
+        city,
+      }
     },
-    Mutation: {
-        addPerson: (root, args) => {
-            if (PermissionStatus.find(p => p.name === args.name)) {
-                throw new UserInputError('Name must be unique', {
-                    invaliArgs: args.name
-                })
-            }
-            const person = { ...args, id: uuid() }
-            persons = persons.concat(person)
-            return person
-        },
-        editNumber: (root, args) => {
-            const person = persons.find(p => p.name === args.name)
-            if (!person) {
-                return null
-            }
-            const updatedPerson = { ...person, phone: args.phone }
-            persons = persons.map(p => p.name === args.name ? updatedPerson : p)
-            return updatedPerson
-        }
-    }
+  },
+  Mutation: {
+    addPerson: (root, args) => {
+      if (persons.find((p) => p.name === args.name)) {
+        throw new UserInputError('Name must be unique', {
+          invalidArgs: args.name,
+        })
+      }
+
+      const person = { ...args, id: uuid() }
+      persons = persons.concat(person)
+      return person
+    },
+    editNumber: (root, args) => {
+      const person = persons.find((p) => p.name === args.name)
+      if (!person) {
+        return null
+      }
+
+      const updatedPerson = { ...person, phone: args.phone }
+      persons = persons.map((p) => (p.name === args.name ? updatedPerson : p))
+      return updatedPerson
+    },
+  },
 }
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
 
 server.listen().then(({ url }) => {
-    console.log(`server ready at ${usrl}`);
+  console.log(`Server ready at ${url}`)
 })
